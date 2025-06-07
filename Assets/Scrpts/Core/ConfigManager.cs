7df900ec3a8afe5c4e3e82db9969079deecff6da
nostcoper <carlosserrato200475@gmail.com>
2025-06-07 00:37:49 -0500

    Menu

--- Assets/Scrpts/Core/ConfigManager.cs
+++ Assets/Scrpts/Core/ConfigManager.cs
@@ -1,32 +1,13 @@
 using UnityEngine;
+using Fusion;
 
 public class ConfigManager : Singleton<ConfigManager>
 {
-    [SerializeField] private int playerNumber = 2;
-    [SerializeField] private int limitPlayerNumber = 2;
-    [SerializeField] private int winPoint = 3;
+    [SerializeField] private GameMode _mode = GameMode.AutoHostOrClient;
 
-    public const int MINPLAYERS = 2;
-    public const int MAXPLAYERS = 4;
-    public const int MINPOINTS = 3;
-    public const int MAXPOINTS = 15;
-
-    public int PlayerNumber 
-    { 
-        get => playerNumber; 
-        set => playerNumber = Mathf.Clamp(value, MINPLAYERS, MAXPLAYERS); 
-    }
-
-    public int LimitPlayerNumber 
-    { 
-        get => limitPlayerNumber; 
-        set => limitPlayerNumber = Mathf.Clamp(value, MINPLAYERS, MAXPLAYERS); 
-    }
-
-    public int WinPoint 
-    { 
-        get => winPoint; 
-        set => winPoint = Mathf.Clamp(value, MINPOINTS, MAXPOINTS); 
+    public GameMode Mode
+    {
+        get => _mode;
+        set => _mode = value;
     }
 }
-
